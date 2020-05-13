84bb182b8d92d6aa1e37f2809e3ae05ead01746d
kampa <psol@yandex.ru>
2020-05-13 16:28:56 +0300

    посадки готовы

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -2,26 +2,23 @@
 
 # (if/elif/else)
 
-# Заданы размеры envelop_x, envelop_y - размеры конверта и paper_x, paper_y листа бумаги
-#
-# Определить, поместится ли бумага в конверте (стороны листа параллельны сторонам конверта,
-# если размеры равны - лист входит в конверт впритирку)
-# Не забывайте, что лист бумаги можно перевернуть и попробовать вставить в конверт другой стороной.
-# Результат проверки вывести на консоль (ДА/НЕТ)
-# Использовать только операторы if/elif/else, можно вложенные
-
 envelop_x, envelop_y = 10, 7
-paper_x, paper_y = 8, 9
+# paper_x, paper_y = 8, 9
 # проверить для
 # paper_x, paper_y = 9, 8
 # paper_x, paper_y = 6, 8
 # paper_x, paper_y = 8, 6
 # paper_x, paper_y = 3, 4
 # paper_x, paper_y = 11, 9
-# paper_x, paper_y = 9, 11
-# (просто раскоментировать нужную строку и проверить свой код)
+paper_x, paper_y = 9, 11
 
-# TODO здесь ваш код
+
+if envelop_x >= paper_x and envelop_y >= paper_y:
+    print("Да")
+elif envelop_y >= paper_x or envelop_y >= paper_y:
+    print("Да")
+else:
+    print("Нет")
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -29,8 +26,8 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
+hole_x, hole_y = 8, 9
+#brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
@@ -47,7 +44,16 @@ paper_x, paper_y = 8, 9
 # brick_x, brick_y, brick_z = 6, 11, 3
 # brick_x, brick_y, brick_z = 6, 3, 11
 # brick_x, brick_y, brick_z = 3, 6, 11
-# brick_x, brick_y, brick_z = 3, 11, 6
+brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if hole_x >= brick_x and hole_y >= brick_y:
+    print("Да")
+elif hole_y >= brick_x or hole_x >= brick_y:
+    print("Да")
+elif hole_x >= brick_z and hole_y >= brick_y:
+    print("да")
+elif hole_y >= brick_z and hole_x >= brick_y:
+    print("да")
+else:
+    print("нет")
