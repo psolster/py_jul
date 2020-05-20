10308ade94ec5410fa5218bd435ee892b5e2ae07
kampa <psol@yandex.ru>
2020-05-20 16:20:06 +0300

    не веселые

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -1,13 +1,35 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import random
 
-# Написать функцию отрисовки смайлика по заданным координатам
-# Форма рожицы-смайлика на ваше усмотрение
-# Параметры функции: кордината X, координата Y, цвет.
-# Вывести 10 смайликов в произвольных точках экрана.
+import simple_draw as sd
 
-# TODO здесь ваш код
 
-simple_draw.pause()
+def smile(x: int, y: int, color):
+    sd.resolution = (1200, 600)
+    center = sd.get_point(x, y)
+    left_eye = sd.get_point(x - 15, y + 10)
+    right_eye = sd.get_point(x + 15, y + 10)
+    x1 = x - 50
+    y1 = y - 25
+    x2 = x + 50
+    y2 = y + 25
+    left_bottom = sd.get_point(x1, y1)
+    right_top = sd.get_point(x2, y2)
+    right_top_mouth = sd.get_point(x - 15, y - 15)
+    left_bottom_mouth = sd.get_point(x + 15, y - 10)
+
+    sd.ellipse(left_bottom, right_top, color, 2)
+    sd.circle(center, 5, color, 0)
+    sd.circle(left_eye, 5, color, 1)
+    sd.circle(right_eye, 5, color, 1)
+    sd.rectangle(right_top_mouth, left_bottom_mouth, color, 1)
+
+
+for _ in range(10):
+    x = random.randint(100, 1200)
+    y = random.randint(100, 600)
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    smile(x, y, color)
+sd.pause()
