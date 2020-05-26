ba408b85f1cc24350af2efc0660e4ac0a66bdeb3
kampa <psol@yandex.ru>
2020-05-26 09:58:18 +0300

    заменил звания переменных что бы не пересекались

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -12,26 +12,26 @@ for _ in range(3):
     sd.circle(center_position=point, radius=radius, width=2)
 
 
-def buble(point, step, color):
-    radius = 50
+def buble(central_point, step_radius, color_bubl):
+    start_radius = 50
     for _ in range(3):
-        radius += step
-        sd.circle(center_position=point, radius=radius, color=color, width=2)
+        start_radius += step_radius
+        sd.circle(center_position=central_point, radius=radius, color=color_bubl, width=2)
 
 
 point = sd.get_point(200, 200)
-buble(point=point, step=10, color=(40, 120, 200))
+buble(central_point=point, step_radius=10, color_bubl=(40, 120, 200))
 
 for x in range(100, 1100, 100):
     point = sd.get_point(x, 100)
-    buble(point=point, step=5, color=(40, 120, 200))
+    buble(central_point=point, step_radius=5, color_bubl=(40, 120, 200))
 
 
 for y in range(100, 400, 100):
     for x in range(100, 1100, 100):
         point = sd.get_point(x, y)
         color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        buble(point=point, step=5, color=color)
+        buble(central_point=point, step_radius=5, color_bubl=color)
 
 point = sd.get_point(200, 200)
 
@@ -40,6 +40,6 @@ for _ in range(100):
     point = sd.random_point()
     step = random.randint(2, 20)
     color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    buble(point=point, step=step, color=color)
+    buble(central_point=point, step_radius=step, color_bubl=color)
 
 sd.pause()
