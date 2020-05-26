535becca5215346cb3529bc1b4168ca8e9ec87d4
kampa <psol@yandex.ru>
2020-05-26 14:30:57 +0300

    да, 6 условий

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -15,7 +15,7 @@ paper_x, paper_y = 9, 11
 
 if envelop_x >= paper_x and envelop_y >= paper_y:
     print("Да")
-elif envelop_y >= paper_x or envelop_y >= paper_y:
+elif envelop_y >= paper_x or envelop_y >= paper_x:
     print("Да")
 else:
     print("Нет")
@@ -27,7 +27,7 @@ else:
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
 hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
+#brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
@@ -44,16 +44,20 @@ hole_x, hole_y = 8, 9
 # brick_x, brick_y, brick_z = 6, 11, 3
 # brick_x, brick_y, brick_z = 6, 3, 11
 # brick_x, brick_y, brick_z = 3, 6, 11
-# brick_x, brick_y, brick_z = 3, 11, 6
-# (просто раскоментировать нужную строку и проверить свой код)
+brick_x, brick_y, brick_z = 3, 11, 6
+
 
 if hole_x >= brick_x and hole_y >= brick_y:
     print("Да")
-elif hole_y >= brick_x or hole_x >= brick_y:
+elif hole_x >= brick_y and hole_y >= brick_x:
     print("Да")
+elif hole_x >= brick_x and hole_y >= brick_z:
+    print("да")
+elif hole_x >= brick_z and hole_y >= brick_x:
+    print("да")
+elif hole_x >= brick_y and hole_y >= brick_z:
+    print("да")
 elif hole_x >= brick_z and hole_y >= brick_y:
     print("да")
-elif hole_y >= brick_z and hole_x >= brick_y:
-    print("да")
 else:
-     print("нет")
+    print("нет")
