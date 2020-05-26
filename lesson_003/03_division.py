0ff34a3d98853ebdc47e1d730c4c893c2b5cf6fc
kampa <psol@yandex.ru>
2020-05-26 14:41:10 +0300

    дсадная оплошность, поправил

--- lesson_003/03_division.py
+++ lesson_003/03_division.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-a, b = 179, 37
+
+a, b = 25, 4
 c = a
 i = 0
-while a > b:
+while a >= b:
     a = a - b
     i += 1
 print("Целочисленное деление ", c, "на ", b, "дает ", i)
