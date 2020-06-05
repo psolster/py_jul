21e12d69d9cf570173a2cc59060246b678219852
kampa <psol@yandex.ru>
2020-06-05 20:14:05 +0300

    нет, не интересно

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -7,7 +7,6 @@ import simple_draw as sd
 rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
-# Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 
 sd.resolution = (1200, 600)
 x_1 = 50
@@ -26,9 +25,6 @@ for color in rainbow_colors:
     start_point = sd.get_point(x_1, y_1)
     end_point = sd.get_point(x_2, y_2)
 
-# Усложненное задание, делать по желанию.
-# Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
-# поэкспериментировать с параметрами, что бы было красиво
-# TODO здесь ваш код
+# тут все и так понятно, не так интересно с примитивной графикой
 
 sd.pause()
