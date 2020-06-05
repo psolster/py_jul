8ea95314cc524d4c8b3afa55a6552ce46ca6ee9e
kampa <psol@yandex.ru>
2020-06-05 20:12:58 +0300

    все работает, но не пойму, почему ругается шарм

--- lesson_003/05_store.py
+++ lesson_003/05_store.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
-
-# Есть словарь кодов товаров
-
 goods = {
     'Лампа': '12345',
     'Стол': '23456',
     'Диван': '34567',
     'Стул': '45678',
 }
-
-# Есть словарь списков количества товаров на складе.
 
 store = {
     '12345': [
@@ -27,19 +22,15 @@ store = {
         {'quantity': 50, 'price': 100},
         {'quantity': 12, 'price': 95},
         {'quantity': 43, 'price': 97},
-    ],
+    ]
 }
-
-for name in goods:
-    art = goods[name]
+for product_name, product_code in goods.items():
     total_quon = 0
     total_cost = 0
-    inform = store[art]
-    for soder in enumerate(inform):
-        soder_vok = soder[1]
-        total_cost += soder_vok["quantity"] * soder_vok["price"]
-        total_quon += soder_vok["quantity"]
-    print(name, total_quon, "шт", "стоимость", total_cost)
+    for sostav in store[product_code]:
+        total_cost += sostav["quantity"] * sostav["price"]
+        total_quon += sostav["quantity"]
+    print(product_name, total_quon, "шт", "стоимость", total_cost)
 
 
 
