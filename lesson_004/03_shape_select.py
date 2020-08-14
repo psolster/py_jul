1d5e179c5e821deb90abe84dcd5a9ac391bac474
kampa <psol@yandex.ru>
2020-08-15 00:19:31 +0300

    работает все, но смущают замечания шарма по наименованию параметров функций

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -9,113 +9,72 @@ import simple_draw as sd
 # Код функций из упр lesson_004/02_global_color.py скопировать сюда
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
 
+
+def trianlge(point_zero, angle, lenght_tri, color='5'):
+    angle_fig = [angle + 0, angle + 120, angle + 240]
+    v = sd.get_vector(start_point=point_zero, angle=angle_fig[0], length=lenght_tri, width=3)
+    v.draw(color_maps[color])
+    for i in range(1, 3):
+        v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght_tri, width=3)
+        v.draw(color_maps[color])
+
+
+def kub(point_zero, angle, lenght, color="5"):
+    angle_fig = [angle + 0, angle + 90, angle + 180, angle + 270]
+    v = sd.get_vector(start_point=point_zero, angle=angle_fig[0], length=lenght, width=3)
+    v.draw(color_maps[color])
+    for i in range(1, 4):
+        v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght, width=3)
+        v.draw(color_maps[color])
+
+
+def pentakl(point_zero, angle, lenght, color="5"):
+    angle_fig = [angle + 0, angle + 72, angle + 144, angle + 144 + 72, angle + 144 + 72 + 72]
+    v = sd.get_vector(start_point=point_zero, angle=angle_fig[0], length=lenght, width=3)
+    v.draw(color_maps[color])
+    for i in range(1, 5):
+        v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght, width=3)
+        v.draw(color_maps[color])
+
+
+def hexagon(point_zero, angle, lenght, color="5"):
+    angle_fig = [angle + 0, angle + 60, angle + 120, angle + 180, angle + 240, angle + 300]
+    v = sd.get_vector(start_point=point_zero, angle=angle_fig[0], length=lenght, width=3)
+    v.draw(color_maps[color])
+    for i in range(1, 6):
+        v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght, width=3)
+        v.draw(color_maps[color])
+
+
 sd.resolution = (600, 600)
-point_zer_triangle = sd.get_point(300, 300)
-point_zer_kub = sd.get_point(300, 300)
-point_zer_pent = sd.get_point(300, 300)
-point_zer_hex = sd.get_point(300, 300)
-dlinna_vector = 120
+point_zero = sd.get_point(300, 300)
+lenght = 120
 angle = 0
 
-color_maps = {1: (255, 0, 0),
-              2: (255, 127, 0),
-              3: (255, 255, 0),
-              4: (0, 255, 0),
-              5: (0, 255, 255),
-              6: (0, 0, 255),
-              7: (255, 0, 255)
+
+color_maps = {'1': (255, 0, 0),
+              '2': (255, 127, 0),
+              '3': (255, 255, 0),
+              '4': (0, 255, 0),
+              '5': (0, 255, 255),
+              '6': (0, 0, 255),
+              '7': (255, 0, 255)
               }
 
-
-# TODO Нужно разместить функции над остальным кодом.
-def trianlge(point_zero, angle_tr, lenght_tri, line_color):
-    color = color_maps[line_color]
-    v1 = sd.get_vector(start_point=point_zero, angle=angle_tr, length=lenght_tri, width=3)
-    v1.draw(color=color)
-    angle_tr += 120
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle_tr, length=lenght_tri, width=3)
-    v2.draw(color=color)
-    angle_tr += 120
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle_tr, length=lenght_tri, width=3)
-    v3.draw(color=color)
-
-
-def kub(point_zer, angle_kub, lenght, line_color):
-    color = color_maps[line_color]
-    v1_kub = sd.get_vector(start_point=point_zer, angle=angle_kub, length=lenght, width=3)
-    v1_kub.draw(color=color)
-    angle_kub += 90
-    v2_kub = sd.get_vector(start_point=v1_kub.end_point, angle=angle_kub, length=lenght, width=3)
-    v2_kub.draw(color=color)
-    angle_kub += 90
-    v3_kub = sd.get_vector(start_point=v2_kub.end_point, angle=angle_kub, length=lenght, width=3)
-    v3_kub.draw(color=color)
-    angle_kub += 90
-    v4_kub = sd.get_vector(start_point=v3_kub.end_point, angle=angle_kub, length=lenght, width=3)
-    v4_kub.draw(color=color)
-
-
-def pentakl(point_zer, angle_pent, lenght, line_color):
-    color = color_maps[line_color]
-    v1_pentakl = sd.get_vector(start_point=point_zer, angle=angle_pent, length=lenght, width=3)
-    v1_pentakl.draw(color=color)
-    angle_pent += 72
-    v2_pentakl = sd.get_vector(start_point=v1_pentakl.end_point, angle=angle_pent, length=lenght, width=3)
-    v2_pentakl.draw(color=color)
-    angle_pent += 72
-    v3_pentakl = sd.get_vector(start_point=v2_pentakl.end_point, angle=angle_pent, length=lenght, width=3)
-    v3_pentakl.draw(color=color)
-    angle_pent += 72
-    v4_pentakl = sd.get_vector(start_point=v3_pentakl.end_point, angle=angle_pent, length=lenght, width=3)
-    v4_pentakl.draw(color=color)
-    angle_pent += 72
-    sd.line(start_point=v4_pentakl.end_point, end_point=v1_pentakl.start_point, width=3, color=color)
-
-
-def hexagon(point_zer, angle_hex, lenght, line_color):
-    color = color_maps[line_color]
-    v1_hex = sd.get_vector(start_point=point_zer, angle=angle_hex, length=lenght, width=3)
-    v1_hex.draw(color=color)
-    angle_hex += 60
-    v2_hex = sd.get_vector(start_point=v1_hex.end_point, angle=angle_hex, length=lenght, width=3)
-    v2_hex.draw(color=color)
-    angle_hex += 60
-    v3_hex = sd.get_vector(start_point=v2_hex.end_point, angle=angle_hex, length=lenght, width=3)
-    v3_hex.draw(color=color)
-    angle_hex += 60
-    v4_hex = sd.get_vector(start_point=v3_hex.end_point, angle=angle_hex, length=lenght, width=3)
-    v4_hex.draw(color=color)
-    angle_hex += 60
-    v5_hex = sd.get_vector(start_point=v4_hex.end_point, angle=angle_hex, length=lenght, width=3)
-    v5_hex.draw(color=color)
-    angle_hex += 60
-    sd.line(start_point=v5_hex.end_point, end_point=v1_hex.start_point, width=3, color=color)
-
-
-a = int(input('''Укажите № желаемой фигуры. Фигуры на выбор:
+all_forms = [trianlge, kub, pentakl, hexagon]
+draw_function = {'1': 'Треугольник', '2': 'Квадрат', '3': '5-ти угольник', '4': '6-ти угольник'}
+a = input('''Укажите № желаемой фигуры. Фигуры на выбор:
       1. Трегольник
       2. Квадрат
       3. 5-ти угольник
       4. 6-ти угольник
       
-Ваш выбор-> '''))
+Ваш выбор-> ''')
 
-# TODO Будет удобнее создать словарь в значениях которого будут храниться
-#  названием фигур с ссылками на функции, а ключами словаря будут цифры,
-#  вводимые пользователем.
-#  Ссылки на функции, проще говоря их названия, можно поместить в структуру данных:
-#  список, кортеж, словарь, ... На примере списков это работает так:
-#  functs = [pentagon, hexagon, triangle]
-#  draw_function = functs[0]
-#  draw_function(start_point, start_angle, length
-if a == 1:
-    trianlge(point_zero=point_zer_triangle, angle_tr=angle, lenght_tri=dlinna_vector, line_color=a)
-elif a == 2:
-    kub(point_zer=point_zer_kub, angle_kub=angle, lenght=dlinna_vector, line_color=a)
-elif a == 3:
-    pentakl(point_zer=point_zer_pent, angle_pent=angle, lenght=dlinna_vector, line_color=a)
-elif a == 4:
-    hexagon(point_zer=point_zer_hex, angle_hex=angle, lenght=dlinna_vector, line_color=a)
+
+if a in draw_function:
+    draw_function = all_forms[int(a)-1]
+    draw_function(point_zero, angle, lenght)
 else:
     print('ну не попал в нужный')
     exit()
