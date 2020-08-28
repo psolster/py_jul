c6bf1932d79376d78f6a491e083cdbc7f29967c9
kampa <psol@yandex.ru>
2020-08-28 14:01:29 +0300

    поправил словарь, не понял к чему относится замечание "и сделать цикл по словарю"

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -12,44 +12,50 @@ import simple_draw as sd
 # sd.get_point()
 # sd.line()
 # sd.get_vector()
-# и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
+COLOR_RED = sd.COLOR_RED
+COLOR_ORANGE = sd.COLOR_ORANGE
+COLOR_YELLOW = sd.COLOR_YELLOW
+COLOR_GREEN = sd.COLOR_GREEN
+COLOR_CYAN = sd.COLOR_CYAN
+COLOR_BLUE = sd.COLOR_BLUE
+COLOR_PURPLE = sd.COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
 
 def trianlge(point_zero, angle_tr, lenght_tri, color):
     angle_fig = [angle_tr + 0, angle_tr + 120, angle_tr + 240]
     v = sd.get_vector(start_point=point_zero, angle=angle_fig[0], length=lenght_tri, width=3)
-    v.draw(color_maps[a])
+    v.draw(color)
     for i in range(1, 3):
         v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght_tri, width=3)
-        v.draw(color_maps[color])
+        v.draw(color)
 
 
 def kub(point_zero, angle_kub, lenght, color):
     angle_fig = [angle_kub + 0, angle_kub + 90, angle_kub + 180, angle_kub + 270]
     v = sd.get_vector(start_point=point_zero, angle=angle_fig[0], length=lenght, width=3)
-    v.draw(color_maps[color])
+    v.draw(color)
     for i in range(1, 4):
         v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght, width=3)
-        v.draw(color_maps[color])
+        v.draw(color)
 
 
 def pentakl(point_zero, angle_pent, lenght, color):
     angle_fig = [angle_pent + 0, angle_pent + 72, angle_pent + 144, angle_pent + 144 + 72, angle_pent + 144 + 72 + 72]
     v = sd.get_vector(start_point=point_zero, angle=angle_fig[0], length=lenght, width=3)
-    v.draw(color_maps[color])
+    v.draw(color)
     for i in range(1, 5):
         v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght, width=3)
-        v.draw(color_maps[color])
+        v.draw(color)
 
 
 def hexagon(point_zero, angle_hex, lenght, color):
     angle_fig = [angle_hex + 0, angle_hex + 60, angle_hex + 120, angle_hex + 180, angle_hex + 240, angle_hex + 300]
     v = sd.get_vector(start_point=point_zero, angle=angle_fig[0], length=lenght, width=3)
-    v.draw(color_maps[color])
+    v.draw(color)
     for i in range(1, 6):
         v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght, width=3)
-        v.draw(color_maps[color])
+        v.draw(color)
 
 
 sd.resolution = (1200, 800)
@@ -60,15 +66,13 @@ point_zer_hex = sd.get_point(400, 400)
 dlinna_vector = 80
 angle = 0
 
-# TODO Добавьте в словарь color_maps названия цветов
-#  и используйте цикл по словарю для вывода сообщений.
-color_maps = {'1': (255, 0, 0),
-              '2': (255, 127, 0),
-              '3': (255, 255, 0),
-              '4': (0, 255, 0),
-              '5': (0, 255, 255),
-              '6': (0, 0, 255),
-              '7': (255, 0, 255)
+color_maps = {'1': COLOR_RED,
+              '2': COLOR_ORANGE,
+              '3': COLOR_YELLOW,
+              '4': COLOR_GREEN,
+              '5': COLOR_CYAN,
+              '6': COLOR_BLUE,
+              '7': COLOR_PURPLE
               }
 
 a = input('''Укажите № желаемого цвета фигуры. Цвета на выбор:
@@ -80,11 +84,12 @@ a = input('''Укажите № желаемого цвета фигуры. Цв
       6. Синий
       7. Фиолетовый
 Ваш выбор-> ''')
+
 if a in color_maps:
-    trianlge(point_zero=point_zer_triangle, angle_tr=angle, lenght_tri=dlinna_vector, color=a)
-    kub(point_zero=point_zer_kub, angle_kub=angle, lenght=dlinna_vector, color=a)
-    pentakl(point_zero=point_zer_pent, angle_pent=angle, lenght=dlinna_vector, color=a)
-    hexagon(point_zero=point_zer_hex, angle_hex=angle, lenght=dlinna_vector, color=a)
+    trianlge(point_zero=point_zer_triangle, angle_tr=angle, lenght_tri=dlinna_vector, color=color_maps[a])
+    kub(point_zero=point_zer_kub, angle_kub=angle, lenght=dlinna_vector, color=color_maps[a])
+    pentakl(point_zero=point_zer_pent, angle_pent=angle, lenght=dlinna_vector, color=color_maps[a])
+    hexagon(point_zero=point_zer_hex, angle_hex=angle, lenght=dlinna_vector, color=color_maps[a])
 else:
     print('ну не попал в нужный')
     exit()
