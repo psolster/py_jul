dd2af11792d9c614ca7998bad88f8dc748ccf8b0
kampa <psol@yandex.ru>
2020-09-04 11:21:55 +0300

    готово, если я правильно понял условие

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -46,15 +46,50 @@ import simple_draw as sd
 # draw_branches(start_point_brench=start_point, angle_brench=angle, lenght_brench=lenght)
 #
 
+# def draw_branches(start_point_brench, angle_brench, lenght_brench):
+#     if lenght_brench < 10:
+#         return
+#
+#     v1 = sd.get_vector(start_point_brench, angle_brench, lenght_brench, width=1)
+#     v1.draw()
+#     delta_angle_1 = angle_brench - 30
+#     delta_angle_2 = angle_brench + 30
+#     next_lenght_branches = lenght_brench * .75
+#     draw_branches(start_point_brench=v1.end_point, angle_brench=delta_angle_1, lenght_brench=next_lenght_branches)
+#     draw_branches(start_point_brench=v1.end_point, angle_brench=delta_angle_2, lenght_brench=next_lenght_branches)
+#
+#
+# sd.resolution = (1200, 600)
+# start_point = sd.get_point(600, 30)
+# angle = 90
+# lenght = 100
+
+
+# draw_branches(start_point_brench=start_point, angle_brench=angle, lenght_brench=lenght)
+
+# 3) Запустить вашу рекурсивную функцию, используя следующие параметры:
+# root_point = sd.get_point(300, 30)
+# draw_branches(start_point=root_point, angle=90, length=100)
+# draw_branches(start_point_brench=start_point, angle_brench=angle, lenght_brench=lenght)
+
+
+# 4) Усложненное задание (делать по желанию)
+# - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
+# - сделать рандомное отклонение длины ветвей в пределах 20% от коэффициента 0.75
+# Возможный результат решения см lesson_004/results/exercise_04_fractal_02.jpg
+
+
 def draw_branches(start_point_brench, angle_brench, lenght_brench):
-    if lenght_brench < 10:
+    if lenght_brench < 2:
         return
 
     v1 = sd.get_vector(start_point_brench, angle_brench, lenght_brench, width=1)
     v1.draw()
-    delta_angle_1 = angle_brench - 30
-    delta_angle_2 = angle_brench + 30
-    next_lenght_branches = lenght_brench * .75
+    deviation_size = 30-(30 * (sd.random_number(0, 40) / 100))
+    deviation_size_lenght = .75 - (.75 * (sd.random_number(0, 40) / 100))
+    delta_angle_1 = angle_brench - deviation_size
+    delta_angle_2 = angle_brench + deviation_size
+    next_lenght_branches = lenght_brench * deviation_size_lenght
     draw_branches(start_point_brench=v1.end_point, angle_brench=delta_angle_1, lenght_brench=next_lenght_branches)
     draw_branches(start_point_brench=v1.end_point, angle_brench=delta_angle_2, lenght_brench=next_lenght_branches)
 
@@ -64,20 +99,7 @@ start_point = sd.get_point(600, 30)
 angle = 90
 lenght = 100
 
-
-# draw_branches(start_point_brench=start_point, angle_brench=angle, lenght_brench=lenght)
-
-# 3) Запустить вашу рекурсивную функцию, используя следующие параметры:
-# root_point = sd.get_point(300, 30)
-# draw_branches(start_point=root_point, angle=90, length=100)
 draw_branches(start_point_brench=start_point, angle_brench=angle, lenght_brench=lenght)
-
-# TODO Попробуйте сделать усложнённую часть задачи. Если не хотите её делать,
-#  напишите и я поставлю вам зачёт.
-# 4) Усложненное задание (делать по желанию)
-# - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
-# - сделать рандомное отклонение длины ветвей в пределах 20% от коэффициента 0.75
-# Возможный результат решения см lesson_004/results/exercise_04_fractal_02.jpg
 
 # Пригодятся функции
 # sd.random_number()
