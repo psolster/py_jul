1a4877f510ef75586d69c9c8e822da03e669cecf
kampa <psol@yandex.ru>
2020-09-04 12:11:35 +0300

    снежинки не прерываются, но не пойму как обойти предупреждение системы пока

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -11,28 +11,6 @@ sd.resolution = (1200, 600)
 
 N = 20
 
-# TODO Удобнее хранить координаты и параметры снежинок в одной структуре данных.
-#  Так работать с ними будет проще:
-#  список со списками:
-#  snowflakes = [[0, 2, 4], [5, 6, 7], ...]
-#  for x, y, length in snowflakes:
-#      point = Point(x, y)
-#      snowflake(point, length)
-#  или список со словарями:
-#  snowflakes = [{"x": 0, "y": 2, "length": 4}, {"x": 5, "y": 6, "length": 7}, ]
-#  for snowflake in snowflakes:
-#      point = Point(snowflake['x'], snowflake['y'])
-#      snowflake(point, snowflake["length"])
-#  или словарь со словарями:
-#  snowflakes = {1: {"x": 0, "y": 2, "length": 4}, {"x": 5, "y": 6, "length": 7}, }
-#  for i, snowflake in snowflakes.items():
-#      point = Point(snowflake['x'], snowflake['y'])
-#      snowflake(point, snowflake["length"])
-#  Или с использованием enumerate, если нужен индекс элемента в списке:
-#  for i, (x, y, length) in enumerate(snowflake_param):
-#  Пример для словаря, содержащего список параметров снежинки:
-#  for i, (x, y, length) in snowflake_param.items():
-
 # coordin_x = {}
 # lenght_snowflake_line = {}
 # for key_x_coordinat in range(0, 1201, 60):
@@ -65,7 +43,6 @@ N = 20
 #     немного поспать
 #     если пользователь хочет выйти
 #       прервать цикл
-# TODO Переходите ко второй части задания.
 
 
 # Часть 2 (делается после зачета первой части)
@@ -90,19 +67,20 @@ for j in range(0, N):
 
 while True:
     sd.start_drawing()
-    # TODO Снегопад должен продолжаться до тех пор пока пользователь его не прервёт.
-    #  Снежинки, достигшие нижней части экрана, нужно перемещать наверх или заменять новые.
+
     for i, (x, y, lenght) in enumerate(snowflake_data):
 
         point_background_color = sd.get_point(x, y)
         sd.snowflake(center=point_background_color, length=lenght, color=sd.background_color)
         y = y - 10
         snowflake_data[i][1] = y
-        # TODO Названия переменных принято записывать строчными буквами.
-        point_COLOR_WHITE = sd.get_point(x, y)
-        sd.snowflake(center=point_COLOR_WHITE, length=lenght, color=sd.COLOR_WHITE)
+        point_color_white = sd.get_point(x, y)
+        sd.snowflake(center=point_color_white, length=lenght, color=sd.COLOR_WHITE)
     sd.finish_drawing()
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
+    elif snowflake_data[i][1] <= 20:
+        for j in range(0, N):
+            snowflake_data.append([sd.random_number(0, 1201), sd.random_number(450, 600), sd.random_number(10, 100)])
 sd.pause()
