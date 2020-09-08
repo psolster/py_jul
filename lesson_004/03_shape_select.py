bccf0285d0b9f421020fdb6aaf1332d814b43438
kampa <psol@yandex.ru>
2020-09-08 16:42:21 +0300

    поправил

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -10,52 +10,40 @@ import simple_draw as sd
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
 
 
-def trianlge(point_zero, angle, lenght_tri, color='5'):
-    angle_fig = [angle + 0, angle + 120, angle + 240]
-    v = sd.get_vector(start_point=point_zero, angle=angle_fig[0], length=lenght_tri, width=3)
+def trianlge(point_zero_fig, angle_figu, lenght_tri_fig, color='5'):
+    angle_fig = [angle_figu + 0, angle + 120, angle + 240]
+    v = sd.get_vector(start_point=point_zero_fig, angle=angle_fig[0], length=lenght_tri_fig, width=3)
     v.draw(color_maps[color])
     for i in range(1, 3):
-        v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght_tri, width=3)
+        v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght_tri_fig, width=3)
         v.draw(color_maps[color])
 
 
-def kub(point_zero, angle, lenght, color="5"):
-    angle_fig = [angle + 0, angle + 90, angle + 180, angle + 270]
-    v = sd.get_vector(start_point=point_zero, angle=angle_fig[0], length=lenght, width=3)
+def kub(point_zero_fig, angle_figu, lenght_tri_fig, color="5"):
+    angle_fig = [angle_figu + 0, angle + 90, angle + 180, angle + 270]
+    v = sd.get_vector(start_point=point_zero_fig, angle=angle_fig[0], length=lenght_tri_fig, width=3)
     v.draw(color_maps[color])
     for i in range(1, 4):
-        v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght, width=3)
+        v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght_tri_fig, width=3)
         v.draw(color_maps[color])
 
 
-def pentakl(point_zero, angle, lenght, color="5"):
-    angle_fig = [angle + 0, angle + 72, angle + 144, angle + 144 + 72, angle + 144 + 72 + 72]
-    v = sd.get_vector(start_point=point_zero, angle=angle_fig[0], length=lenght, width=3)
+def pentakl(point_zero_fig, angle_figu, lenght_tri_fig, color="5"):
+    angle_fig = [angle_figu + 0, angle + 72, angle + 144, angle + 144 + 72, angle + 144 + 72 + 72]
+    v = sd.get_vector(start_point=point_zero_fig, angle=angle_fig[0], length=lenght_tri_fig, width=3)
     v.draw(color_maps[color])
     for i in range(1, 5):
-        v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght, width=3)
+        v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght_tri_fig, width=3)
         v.draw(color_maps[color])
 
 
-def hexagon(point_zero, angle, lenght, color="5"):
-    angle_fig = [angle + 0, angle + 60, angle + 120, angle + 180, angle + 240, angle + 300]
-    v = sd.get_vector(start_point=point_zero, angle=angle_fig[0], length=lenght, width=3)
+def hexagon(point_zero_fig, angle_figu, lenght_tri_fig, color="5"):
+    angle_fig = [angle_figu + 0, angle + 60, angle + 120, angle + 180, angle + 240, angle + 300]
+    v = sd.get_vector(start_point=point_zero_fig, angle=angle_fig[0], length=lenght_tri_fig, width=3)
     v.draw(color_maps[color])
     for i in range(1, 6):
-        v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght, width=3)
+        v = sd.get_vector(start_point=v.end_point, angle=angle_fig[i], length=lenght_tri_fig, width=3)
         v.draw(color_maps[color])
-
-
-COLOR_RED = sd.COLOR_RED
-COLOR_ORANGE = sd.COLOR_ORANGE
-COLOR_YELLOW = sd.COLOR_YELLOW
-COLOR_GREEN = sd.COLOR_GREEN
-COLOR_CYAN = sd.COLOR_CYAN
-COLOR_BLUE = sd.COLOR_BLUE
-COLOR_PURPLE = sd.COLOR_PURPLE
-
-# TODO Обратите внимание на предупреждение среды разработки о том, что внутри функции используются те же имена
-#  переменных, что и вне ее. Здесь это не критично но в других случаях может вызвать ошибки.
 
 
 sd.resolution = (600, 600)
@@ -64,31 +52,29 @@ lenght = 120
 angle = 0
 
 
-color_maps = {'1': COLOR_RED,
-              '2': COLOR_ORANGE,
-              '3': COLOR_YELLOW,
-              '4': COLOR_GREEN,
-              '5': COLOR_CYAN,
-              '6': COLOR_BLUE,
-              '7': COLOR_PURPLE
+color_maps = {'1': sd.COLOR_RED,
+              '2': sd.COLOR_ORANGE,
+              '3': sd.COLOR_YELLOW,
+              '4': sd.COLOR_GREEN,
+              '5': sd.COLOR_CYAN,
+              '6': sd.COLOR_BLUE,
+              '7': sd.COLOR_PURPLE
               }
 
 
-# TODO Жобавьте название фигур в значения словаря draw_function и используйте
-#  этот словарь для вывода сообщений при выборя фигуры.
-draw_function = {'1': trianlge, '2': kub, '3': pentakl, '4': hexagon}
-a = input('''Укажите № желаемой фигуры. Фигуры на выбор:
-      1. Трегольник
-      2. Квадрат
-      3. 5-ти угольник
-      4. 6-ти угольник
-      
-Ваш выбор-> ''')
+draw_function = {'1': ['треугольник', trianlge],
+                 '2': ['Куб', kub],
+                 '3': ['Пятиугольник', pentakl],
+                 '4': ['Шестиугольник', hexagon]}
 
+for key, value in draw_function.items():
+    print(key, value[0])
+
+a = input('Укажите № желаемого цвета фигуры. Цвета на выбор: Ваш выбор-> ')
 
 if a in draw_function:
-    start_function = draw_function[a]
-    start_function(point_zero, angle, lenght)
+    start_function = draw_function[a][1]
+    start_function(point_zero_fig=point_zero, angle_figu=angle, lenght_tri_fig=lenght)
 else:
     print('ну не попал в нужный')
     exit()
