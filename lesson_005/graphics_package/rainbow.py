9240819b05bf95d1446941e716d4152974966562
kampa <psol@yandex.ru>
2020-09-19 23:51:49 +0300

    радуга готова к вызову по точкам

---
+++ lesson_005/graphics_package/rainbow.py
@@ -0,0 +1,26 @@
+import simple_draw as sd
+
+
+def rainbow(start_rainbow_point, end_rainbow_point):
+
+    rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
+    sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
+
+    sd.resolution = (1200, 600)
+
+    start_point = sd.get_point(*start_rainbow_point)
+    end_point = sd.get_point(*end_rainbow_point)
+    x1 = start_rainbow_point[0]
+    x2 = end_rainbow_point[0]
+    for color in rainbow_colors:
+        sd.line(start_point, end_point, color, 4)
+
+        x1 += 5
+        x2 += 5
+        start_point = sd.get_point(x1, start_rainbow_point[1])
+        end_point = sd.get_point(x2, end_rainbow_point[1])
+
+    sd.pause()
+
+
+# rainbow(start_rainbow_point=(50, 50), end_rainbow_point=(300, 300))
