ad31d9399fe863a2e031040d68fc2aa5c99b327b
kampa <psol@yandex.ru>
2020-09-20 15:28:22 +0300

    нормальная стена

--- lesson_005/graphics_package/wall.py
+++ lesson_005/graphics_package/wall.py
@@ -4,7 +4,7 @@ import simple_draw as sd
 
 def wall(start, end):
     color = sd.COLOR_ORANGE
-    sd.resolution = (1200, 600)
+    sd.resolution = (1200, 800)
 
     x_1 = start[0]
     x_2 = end[0]
@@ -27,12 +27,16 @@ def wall(start, end):
             start_point_rect = sd.get_point(x_1_brick, y_1_brick)
             end_point_rect = sd.get_point(x_2_brick, y_2_brick)
             sd.rectangle(start_point_rect, end_point_rect, color, 2)
-        # sd.line(start_point=(x_1, y_1), end_point=(x_1, y_2))
-        # sd.line(start_point=(x_2, y_1), end_point=(x_2, y_2))
+    start_line_1 = sd.get_point(*start)
+    finish_line_1 = sd.get_point(start[0], end[1]+50)
+    sd.line(start_point=start_line_1, end_point=finish_line_1, color=color, width=2)
+    start_line_2 = sd.get_point(end[0]+100, start[1])
+    finish_line_2 = sd.get_point(end[0]+100, end[1]+50)
+    sd.line(start_point=start_line_2, end_point=finish_line_2, color=color, width=2)
 
     sd.pause()
 
 
-start_wall = (0, 0)
-end_wall = (600, 600)
+start_wall = (100, 0)
+end_wall = (600, 300)
 wall(start=start_wall, end=end_wall)
