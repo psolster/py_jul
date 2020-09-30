4eb0062cab279569fc23ba3e66fee912195ef425
kampa <psol@yandex.ru>
2020-09-30 09:40:27 +0300

    утро с солнцем

--- lesson_005/04_painting.py
+++ lesson_005/04_painting.py
@@ -25,7 +25,7 @@ import simple_draw as sd
 from lesson_005.graph_pack import house
 from lesson_005.graph_pack import smile
 from lesson_005.graph_pack import snow_module
-from lesson_005.graph_pack import prim_graph
+# from lesson_005.graph_pack import prim_graph
 
 rainbow.rainbow(start_rainbow_point=(650, 800), end_rainbow_point=(1200, 400))
 tree.draw_branches(start_point_branch=sd.get_point(1000, 130), angle_branch=90, length_branch=100)
@@ -34,7 +34,8 @@ tree.draw_branches(start_point_branch=sd.get_point(1050, 180), angle_branch=90, 
 tree.draw_branches(start_point_branch=sd.get_point(900, 90), angle_branch=90, length_branch=100)
 house.house(center_of_house=(600, 300), width_house=300)
 smile.smile(coordinat_centr=(650, 300), color_smile=sd.COLOR_WHITE)
-prim_graph.primitivs(point_zero_fig=(200, 500), start_angle_figura=10, lenght_line=20, number_of_sides=8)
+# prim_graph.primitivs(point_zero_fig=(200, 500), start_angle_figura=10, lenght_line=20, number_of_sides=8)
+sd.circle(center_position=sd.get_point(200, 500), radius=50, color=sd.COLOR_ORANGE, width=0)
 snow_module.snow_module(left_bottom=(50, 180), right_top=(430, 450), length_snow_line=8, number_snowflake=10)
 
 
