c72027045591fbd7456b6d541f1ed4a9ab9fb638
Pavel <psol@yandex.ru>
2020-11-04 19:00:53 +0300

    на верном пути

--- lesson_006/snowfall.py
+++ lesson_006/snowfall.py
@@ -7,9 +7,14 @@ sd.resolution = (1200, 600)
 
 
 def create_snowfalls(numbers_sn):
-    global _snowflake_data
-    for j in range(0, numbers_sn):
-        _snowflake_data.append([sd.random_number(0, 1201), sd.random_number(450, 600), 10])
+    global _snowflake_data, _down_snowflakes
+    if not _down_snowflakes:
+        for index in _down_snowflakes:
+            _snowflake_data[index][0] = sd.random_number(0, 1201)
+            _snowflake_data[index][1] = sd.random_number(450, 600)
+        else:
+            for j in range(0, numbers_sn):
+                _snowflake_data.append([sd.random_number(0, 1201), sd.random_number(450, 600), 10])
 
 
 def dr_snowflake(color):
@@ -26,27 +31,27 @@ def move_snowflake():
     global _snowflake_data
     for i in range(0, len(_snowflake_data)):
         dr_snowflake(color=sd.background_color)
-        _snowflake_data[i][1] = _snowflake_data[i][1] - 10
+        _snowflake_data[i][1] = _snowflake_data[i][1] - 200
         dr_snowflake(color=sd.COLOR_WHITE)
     return _snowflake_data
 
 
 def get_number_down_snowflakes():
-
     global _snowflake_data, _down_snowflakes
     _down_snowflakes = []
     for i in range(0, len(_snowflake_data)):
-        if _snowflake_data[i][1] < 50:
+        if _snowflake_data[i][1] < 350:
             _down_snowflakes.append(i)
     return _down_snowflakes
 
 
 def del_down_snowflakes(list_index):
-    global _quo_down_snow
+    global _quo_down_snow, _snowflake_data
     _quo_down_snow = 0
+    _quo_down_snow = len(list_index)
+
     for index in list_index:
-        _quo_down_snow += 1
         center = sd.get_point(_snowflake_data[index][0], _snowflake_data[index][1])
         sd.snowflake(center=center, length=10, color=sd.background_color)
-        # _snowflake_data.pop(index)
+
     return _quo_down_snow
