f71d0f01a018c56db843f82fb7bfd57b3156bb50
kampa <psol@yandex.ru>
2020-11-04 17:29:51 +0300

    как удалить

--- lesson_006/02_snowfall_module.py
+++ lesson_006/02_snowfall_module.py
@@ -2,8 +2,7 @@
 
 import simple_draw as sd
 
-from snowfall import create_snowfalls, dr_snowflake, move_snowflake, get_number_down_snowflakes, del_down_snowflakes, \
-    _down_snowflakes, _quon_down_snow
+from snowfall import create_snowfalls, dr_snowflake, move_snowflake, get_number_down_snowflakes, del_down_snowflakes
 
 # На основе кода из lesson_004/05_snowfall.py
 # сделать модуль snowfall.py в котором реализовать следующие функции
@@ -23,9 +22,9 @@ create_snowfalls(numbers_sn=2)
 while True:
     dr_snowflake(color=sd.COLOR_WHITE)
     move_snowflake()
-    get_number_down_snowflakes()
-    del_down_snowflakes(list_index=_down_snowflakes)
-    create_snowfalls(numbers_sn=_quon_down_snow)
+    number_out_snowflakes = get_number_down_snowflakes()
+    quo_new_snowflakes = del_down_snowflakes(list_index=number_out_snowflakes)
+    create_snowfalls(numbers_sn=quo_new_snowflakes)
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
--- lesson_006/snowfall.py
+++ lesson_006/snowfall.py
@@ -2,7 +2,7 @@ import simple_draw as sd
 
 _snowflake_data = []
 _down_snowflakes = []
-_quon_down_snow = 0
+_quo_down_snow = 0
 sd.resolution = (1200, 600)
 
 
@@ -26,23 +26,27 @@ def move_snowflake():
     global _snowflake_data
     for i in range(0, len(_snowflake_data)):
         dr_snowflake(color=sd.background_color)
-        _snowflake_data[i][1] = _snowflake_data[i][1] - 100
+        _snowflake_data[i][1] = _snowflake_data[i][1] - 10
         dr_snowflake(color=sd.COLOR_WHITE)
     return _snowflake_data
 
 
 def get_number_down_snowflakes():
+
     global _snowflake_data, _down_snowflakes
+    _down_snowflakes = []
     for i in range(0, len(_snowflake_data)):
-        if _snowflake_data[i][1] < 300:
-            _down_snowflakes.append(_snowflake_data[i])
+        if _snowflake_data[i][1] < 50:
+            _down_snowflakes.append(i)
     return _down_snowflakes
 
 
 def del_down_snowflakes(list_index):
-    global _quon_down_snow, _down_snowflakes
-    for index, (x, y, length) in enumerate(list_index):
-        _quon_down_snow += 1
-        center_point = sd.get_point(x, y)
-        sd.snowflake(center=center_point, length=length, color=sd.background_color)
-    return _quon_down_snow
+    global _quo_down_snow
+    _quo_down_snow = 0
+    for index in list_index:
+        _quo_down_snow += 1
+        center = sd.get_point(_snowflake_data[index][0], _snowflake_data[index][1])
+        sd.snowflake(center=center, length=10, color=sd.background_color)
+        # _snowflake_data.pop(index)
+    return _quo_down_snow
