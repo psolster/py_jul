b15065b24bd7fca4fdd215b49669606c3675b207
kampa <psol@yandex.ru>
2020-11-14 21:50:27 +0300

    начало снежинок 7 модуля решил =) гениальное - рядом.  Спасибо за подсказку

--- lesson_006/snowfall.py
+++ lesson_006/snowfall.py
@@ -5,15 +5,10 @@ sd.resolution = (1200, 600)
 
 def create_snowfalls(numbers_sn):
     global _snowflake_data
-    if not _snowflake_data:
-        for j in range(0, numbers_sn):
-            _snowflake_data.append([sd.random_number(0, 1201), sd.random_number(250, 600), 10])
-    else:
-        for i, (x, y, length) in enumerate(_snowflake_data):
-            if y < 20:
-                _snowflake_data.pop(i)
-                _snowflake_data.append([sd.random_number(0, 1201), sd.random_number(450, 600), 10])
-        return _snowflake_data
+    for j in range(0, numbers_sn):
+        _snowflake_data.append([sd.random_number(0, 1201), sd.random_number(250, 600), 10])
+
+    return _snowflake_data
 
 
 def dr_snowflake(color):
@@ -38,7 +33,10 @@ def get_number_down_snowflakes():
 
 
 def del_down_snowflakes(list_index):
+    list_index.sort()
+    list_index.reverse()
     for index in list_index:
         center = sd.get_point(_snowflake_data[index][0], _snowflake_data[index][1])
         sd.snowflake(center=center, length=10, color=sd.background_color)
+        _snowflake_data.pop(index)
     return len(list_index)
