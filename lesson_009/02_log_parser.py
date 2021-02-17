7660e6c8aa40059044820cd157f4a84731560785
Solo <psol@yandex.ru>
2021-02-17 19:18:47 +0300

    замена во всех классах работы с файлом, код явно быстрей стал работать

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -42,19 +42,7 @@ class LogParser:
         self.print_to_file(data)
 
     def list_number_lines_w_nok(self):
-        # TODO Пересчитывать количество строк в файле не нужно. Лучше сделать
-        #  цикл for по открытому файлу. Дальше есть два варианта:
-        #  Первый способ:
-        #  Для всех строк, содержащих NOK делать срез фрагмента строки с датой
-        #  и по этому ключу считать количество событий в группе.
-        #  Второй способ:
-        #  Не хранить статистику по всему файлу, а хранить
-        #  строку с текущим временем и количество событий. Если в текущей
-        #  строке время изменилось, нужно записать в файл старое значение
-        #  времени и количество, сбросить счётчик и обновить строку, используемую
-        #  для сравнения.
-        #  Реализация других способов группировки будут отличаться границами среза.
-        #  для каждого из вариантов.
+
         with open(self.filename, 'r', encoding='cp1251') as ff:
             for n, line in enumerate(ff, 1):
                 data = line
@@ -85,49 +73,26 @@ class LogParserHours(LogParser):
                     self.voc_time_nok[time] += 1
         return self.voc_time_nok
 
-        # while self.count_lines != 0:
-        #     ff = open(self.filename, 'r', encoding='cp1251')
-        #     ff.seek(self.position_on_files)
-        #     data = ff.readline()
-        #     self.position_on_files = ff.tell()
-        #     if data[29:] == 'NOK\n':
-        #
-        #         self.voc_time_nok[time] += 1
-        #         self.nok_count += 1
-        #     self.count_lines -= 1
-        #     ff.close()
-        # return self.voc_time_nok
-
 
 class LogParserMonth(LogParser):
     def list_number_lines_w_nok(self):
-        while self.count_lines != 0:
-            ff = open(self.filename, 'r', encoding='cp1251')
-            ff.seek(self.position_on_files)
-            data = ff.readline()
-            self.position_on_files = ff.tell()
-            if data[29:] == 'NOK\n':
-                time = data[0:5] + '-' + data[6:8] + ']'
-                self.voc_time_nok[time] += 1
-                self.nok_count += 1
-            self.count_lines -= 1
-            ff.close()
+        with open(self.filename, 'r', encoding='cp1251') as ff:
+            for n, line in enumerate(ff, 1):
+                data = line
+                if data[29:] == 'NOK\n':
+                    time = data[0:5] + '-' + data[6:8] + ']'
+                    self.voc_time_nok[time] += 1
         return self.voc_time_nok
 
 
 class LogParserYear(LogParser):
     def list_number_lines_w_nok(self):
-        while self.count_lines != 0:
-            ff = open(self.filename, 'r', encoding='cp1251')
-            ff.seek(self.position_on_files)
-            data = ff.readline()
-            self.position_on_files = ff.tell()
-            if data[29:] == 'NOK\n':
-                time = data[0:5] + ']'
-                self.voc_time_nok[time] += 1
-                self.nok_count += 1
-            self.count_lines -= 1
-            ff.close()
+        with open(self.filename, 'r', encoding='cp1251') as ff:
+            for n, line in enumerate(ff, 1):
+                data = line
+                if data[29:] == 'NOK\n':
+                    time = data[0:5] + ']'
+                    self.voc_time_nok[time] += 1
         return self.voc_time_nok
 
 
