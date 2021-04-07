ae5c0d1c943bf1adf762575c35849d77ba5b10df
@kamyshev_pavel <psol@yandex.ru>
2021-04-07 16:31:16 +0300

    trimorphic работает

--- lesson_011/02_prime_numbers.py
+++ lesson_011/02_prime_numbers.py
@@ -74,27 +74,6 @@ def prime_numbers_generator(n):
             yield number
 
 
-# for number in prime_numbers_generator(n=10000):
-#     print(number)
-
-
-# TODO Переходите к третьей части задания.
-# Часть 3
-# Написать несколько функций-фильтров, которые выдает True, если число:
-# 1) "счастливое" в обыденном пониманиии - сумма первых цифр равна сумме последних
-#       Если число имеет нечетное число цифр (например 727 или 92083),
-#       то для вычисления "счастливости" брать равное количество цифр с начала и конца:
-#           727 -> 7(2)7 -> 7 == 7 -> True
-#           92083 -> 92(0)83 -> 9+2 == 8+3 -> True
-# 2) "палиндромное" - одинаково читающееся в обоих направлениях. Например 723327 и 101
-# 3) придумать свою (https://clck.ru/GB5Fc в помощь)
-#
-# Подумать, как можно применить функции-фильтры к полученной последовательности простых чисел
-# для получения, к примеру: простых счастливых чисел, простых палиндромных чисел,
-# простых счастливых палиндромных чисел и так далее. Придумать не менее 2х способов.
-#
-# Подсказка: возможно, нужно будет добавить параметр в итератор/генератор.
-
 def fun_number(number):
     number = str(number)
     lenght = len(number)
@@ -124,11 +103,6 @@ def fun_number(number):
             return False
 
 
-# for number in prime_numbers_generator(n=100000):
-#     res = fun_number(number=number)
-#     print(f'Number-> {number} Fun {res}')
-
-
 def palindromic_number(number):
     number = str(number)
     lenght = len(number)
@@ -140,10 +114,58 @@ def palindromic_number(number):
     list_right.reverse()
     if list_left == list_right:
         return True
+    else:
+        return False
 
 
-for number in prime_numbers_generator(n=100000):
-    if len(str(number)) < 3:
-        continue
-    res = fun_number(number=number)
-    print(f'Number-> {number} Palindromic {res}')
+def trimorphic_number(number):
+    kub_namber = number ** 3
+    number = str(number)
+    kub_namber = str(kub_namber)
+    lenght_kub = len(kub_namber)
+    lenght = len(number)
+    slic = lenght_kub - lenght
+    if kub_namber[slic:] == number:
+        return True
+    else:
+        return False
+
+
+# for number in prime_numbers_generator(n=10000):
+#     print(number)
+
+
+
+# Часть 3
+# Написать несколько функций-фильтров, которые выдает True, если число:
+# 1) "счастливое" в обыденном пониманиии - сумма первых цифр равна сумме последних
+#       Если число имеет нечетное число цифр (например 727 или 92083),
+#       то для вычисления "счастливости" брать равное количество цифр с начала и конца:
+#           727 -> 7(2)7 -> 7 == 7 -> True
+#           92083 -> 92(0)83 -> 9+2 == 8+3 -> True
+# 2) "палиндромное" - одинаково читающееся в обоих направлениях. Например 723327 и 101
+# 3) придумать свою (https://clck.ru/GB5Fc в помощь)
+#
+# Подумать, как можно применить функции-фильтры к полученной последовательности простых чисел
+# для получения, к примеру: простых счастливых чисел, простых палиндромных чисел,
+# простых счастливых палиндромных чисел и так далее. Придумать не менее 2х способов.
+#
+# Подсказка: возможно, нужно будет добавить параметр в итератор/генератор.
+
+
+# for number in prime_numbers_generator(n=100000):
+#     res = fun_number(number=number)
+#     print(f'Number-> {number} Fun {res}')
+
+
+# for number in prime_numbers_generator(n=100000):
+#     if len(str(number)) < 3:
+#         continue
+#     res = palindromic_number(number=number)
+#     print(f'Number-> {number} Palindromic {res}')
+
+
+for number in prime_numbers_generator(n=10000):
+    third_degree = number ** 3
+    res = trimorphic_number(number=number)
+    print(f'Number-> {number} third degree {third_degree} Trimorphic {res}')
