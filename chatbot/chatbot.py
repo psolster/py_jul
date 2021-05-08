e0e46a6e876a9846e10012f8b96ef1ee373e656a
@kamyshev_pavel <psol@yandex.ru>
2021-05-08 22:59:51 +0300

    разобрался с настройками эхо работает

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -12,28 +12,26 @@ class FerstBot:
         self.token = token
         self.vk = vk_api.VkApi(token=token)
         self.long_poller = vk_api.bot_longpoll.VkBotLongPoll(self.vk, self.group_id)
-        # self.api = self.vk.get_api()
+        self.api = self.vk.get_api()
 
     def run(self):
-        print('запустился ран')
         for event in self.long_poller.listen():
-            print('получено событие')
             try:
                 self.on_event(event)
             except Exception as bag:
                 print(bag)
 
+
     def on_event(self, event):
-        print(event)
-        # if events.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
-        #     print(event.type)
-        #     self.api.message.send(
-        #         message=action.object.text,
-        #         random_id=random.randint(1, 2 ** 20),
-        #         peer_id=action.object.peer_id,
-        #     )
-        # else:
-        #     print('пока неумехи', action.type)
+        if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
+            print(event.object.text)
+            self.api.messages.send(
+                message=event.object.text + ' я пока тока эхоо',
+                random_id=randint(1, 2 ** 20),
+                peer_id=event.object.peer_id,
+            )
+        else:
+            print('пока неумехи', event.type)
 
 
 if __name__ == '__main__':
