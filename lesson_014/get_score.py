dac05672d05111c60d48835ef07e9048031a5767
@kamyshev_pavel <psol@yandex.ru>
2021-05-17 22:56:00 +0300

    ищем исключения

--- lesson_014/get_score.py
+++ lesson_014/get_score.py
@@ -7,20 +7,24 @@ result = bowling.result
 
 
 def get_score(game_result):
+    try:
+        prov_game_result = list(game_result)
+        for i, symbol in enumerate(prov_game_result):
+            if symbol == 'X':
+                if prov_game_result[i+1] == '/':
+    except print('не верный формат записи результата, после X не может быть /')
+
     count_stike = game_result.count('X')
     count_second_throw = game_result.count('/')
     list_results = list(game_result)
-
     for i, symb in enumerate(list_results):
         if symb == 'X':
-
             list_results[i] = '0'
         elif symb == '/':
             list_results[i-1] = '0'
             list_results[i] = '0'
         elif symb == '-':
             list_results[i] = '0'
-
     result = [int(item) for item in list_results]
     total_count = count_stike*20+count_second_throw*15+sum(result)
     return total_count
