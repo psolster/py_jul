462f741692397b49fffc09de9d6dfb0afe6c0f90
@kamyshev_pavel <psol@yandex.ru>
2021-05-18 22:56:09 +0300

    истина рядом

--- lesson_014/get_score.py
+++ lesson_014/get_score.py
@@ -11,29 +11,39 @@ class FormatError(Exception):
 
 
 def get_score(game_result):
-    prov_game_result = list(game_result)
-    for i, symbol in enumerate(prov_game_result):
-        if symbol == 'X':
-            try:
-                if prov_game_result[i + 1] == '/':
-                    raise FormatError('Ошибка записи параметра')
-            except (FormatError, IndexError):
-                print('не верный формат записи результата, после X не может быть /')
+    err = error_control(game_result)
+    try:
+        if not err:
 
-    count_stike = game_result.count('X')
-    count_second_throw = game_result.count('/')
-    list_results = list(game_result)
-    for i, symb in enumerate(list_results):
-        if symb == 'X':
-            list_results[i] = '0'
-        elif symb == '/':
-            list_results[i - 1] = '0'
-            list_results[i] = '0'
-        elif symb == '-':
-            list_results[i] = '0'
-    result = [int(item) for item in list_results]
-    total_count = count_stike * 20 + count_second_throw * 15 + sum(result)
+            count_stike = game_result.count('X')
+            count_second_throw = game_result.count('/')
+            list_results = list(game_result)
+            for i, symb in enumerate(list_results):
+                if symb == 'X':
+                    list_results[i] = '0'
+                elif symb == '/':
+                    list_results[i - 1] = '0'
+                    list_results[i] = '0'
+                elif symb == '-':
+                    list_results[i] = '0'
+            result = [int(item) for item in list_results]
+            total_count = count_stike * 20 + count_second_throw * 15 + sum(result)
+    except FormatError:
+        print('не те запси')
     return total_count
+
+
+def error_control(game_result):
+    try:
+        if game_result in '/X':
+            raise FormatError
+    except print('ошибка формата'):
+        print('не прошло')
+
+    return False
+
+
+
 
 
 start = get_score(game_result=result)
